783caa6ffe50472c2d1c3a622383e58cc2d1bacb
Rahul Bera <bera2591@gmail.com>
2020-08-03 02:54:41 +0530

    Locate.dart: Add checkLocationServices method This method asks the user to enable the Location services, if they are not activated.

--- farmerHelperApp/lib/utilities/Locate.dart
+++ farmerHelperApp/lib/utilities/Locate.dart
@@ -1,19 +1,35 @@
 import 'package:geolocator/geolocator.dart';
-class Location
-{
-  double lat,long;
-  void locate() async
-  {
-    try {
-      Position position = await Geolocator().getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.best);
-      lat = position.latitude;
-      long = position.longitude;
-    }
-    catch(e)
-    {
-      print(e);
+import 'package:location/location.dart' as loc;
+
+class Location {
+  double lat, long;
+
+  Location() {
+    checkLocationServices();
+  }
+
+  Future checkLocationServices() async {
+    loc.Location location = loc.Location();
+
+    bool _serviceEnabled;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
     }
   }
 
+  void locate() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+      lat = position.latitude;
+      long = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
 }
